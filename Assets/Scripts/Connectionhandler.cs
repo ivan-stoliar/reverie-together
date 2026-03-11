82a7593e18605798bbd028ba234387e8346f51aa
Ivan Stoliar <ivan.stoliar.data@gmail.com>
2026-03-11 14:52:55 +0200

    Added itchio Tilemaps, created example TilePalletes, implemented basic Fishnet connection, added ParrelSync for testing, requires movement fix

--- Assets/Scripts/Connectionhandler.cs
+++ Assets/Scripts/Connectionhandler.cs
@@ -1,12 +1,78 @@
 using UnityEngine;
+using FishNet;
+using FishNet.Transporting;
+
+public enum ConnectionType
+{
+    Host,
+    Client
+}
+
 
 public class Connectionhandler : MonoBehaviour
 {
+
+    public ConnectionType connectionType;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+#if UNITY_EDITOR
+    private void OnDisable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState -= onClientConnectionState;
+    }
+
+
+    private void OnEnable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState += onClientConnectionState;
+    }
+
+    private void onClientConnectionState(ClientConnectionStateArgs args)
+    {
+        if(args.ConnectionState == LocalConnectionState.Stopping)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        }
+    }
+#endif
     void Start()
     {
+
+        #if UNITY_EDITOR
+        if(ParrelSync.ClonesManager.IsClone())
+        {
+            InstanceFinder.ClientManager.StartConnection();
+
+
+        }
         
+
+        else
+        {
+
+            if (connectionType == ConnectionType.Host) {
+
+                InstanceFinder.ServerManager.StartConnection();
+                InstanceFinder.ClientManager.StartConnection();
+
+            }
+            else
+            {
+                InstanceFinder.ClientManager.StartConnection();
+
+            }
+
+
+        }
+#endif
+
+#if DEDICATED_SERVER
+ InstanceFinder.ServerManager.StartConnection();
+#endif
+
     }
+
+
 
     // Update is called once per frame
     void Update()
